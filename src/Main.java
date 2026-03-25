b7857f57d2f8d328d41fe737ceab6ce697295217
hannidmh <damiche32.62@gmail.com>
2026-03-25 14:58:24 +0100

    init project

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import vuecontroleur.VueControleur;
+import modele.jeu.Jeu;
+
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Jeu jeu = new Jeu();
+                VueControleur vc = new VueControleur(jeu);
+                jeu.getPlateau().addObserver(vc);
+                vc.setVisible(true);
+            }
+        };
+
+        SwingUtilities.invokeLater(r);
+
+        }
+
+
+    }
